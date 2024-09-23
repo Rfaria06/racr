dce74111b4f258b19128e3eff03ffb3a24998e15
Raul Filipe Faria <raul@faria.ch>
2024-09-23 11:53:39 +0200

    reinsert auth buttons

--- src/app/tracks/tracks.ts
+++ src/app/tracks/tracks.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TrackApiService } from '../shared/api/track/track-api.service';
 import { Track } from '../shared/models/track';
 import { RecordModel } from 'pocketbase';
+import { AuthService } from '../shared/api/auth/auth.service';
 
 @Component({
   selector: 'app-tab1',
@@ -9,11 +10,22 @@ import { RecordModel } from 'pocketbase';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor(public api: TrackApiService) {}
+  readonly #authService: AuthService;
+  authenticated: boolean;
+
+  constructor(public api: TrackApiService) {
+    this.#authService = inject(AuthService);
+    this.authenticated = this.#authService.isAuthenticated();
+  }
 
   public tracks: Track[] = [];
 
-  async ngOnInit(){
-    this.tracks = await this.api.getAll()
+  async ngOnInit() {
+    this.tracks = await this.api.getAll();
+  }
+
+  public logout() {
+    this.#authService.logout();
+    window.location.reload();
   }
 }
